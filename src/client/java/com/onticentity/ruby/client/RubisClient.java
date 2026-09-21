91d305771c833ca61b39db903d38d8e0b2889edc
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-09-21 16:32:43 +0200

    init: init fabric project

---
+++ src/client/java/com/onticentity/ruby/client/RubisClient.java
@@ -0,0 +1,10 @@
+package com.onticentity.ruby.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RubisClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/onticentity/ruby/client/RubisDataGenerator.java
@@ -0,0 +1,11 @@
+package com.onticentity.ruby.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RubisDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
